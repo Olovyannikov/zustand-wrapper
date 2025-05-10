17572f645a0dee1f789cc2c2398c2a4151b7a4d7
Ilya Olovyannikov <excaliburgyro@gmail.com>
2025-05-10 15:03:02 +0300

    feat(init): add base file structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import { compression } from 'vite-plugin-compression2';
+import inspect from 'vite-plugin-inspect';
+import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    const isDev = mode === 'development';
+
+    return {
+        plugins: [
+            inspect({
+                build: true,
+                outputDir: '.vite-inspect',
+            }),
+            react(),
+            svgr(),
+            compression(),
+        ],
+        css: {
+            modules: {
+                generateScopedName: isDev ? '[folder]__[local]_[hash:base64:5]' : '[hash:base64:5]',
+            },
+        },
+        ssr: {},
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
+        },
+    };
+});
